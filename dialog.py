30664469aafb8ac36f3471f07f9ff41ea26019b6
eipi10 <gdzjydz@mail.ustc.edu.cn>
2016-01-22 18:46:17 +0800

    gsqd v0.1

---
+++ dialog.py
@@ -0,0 +1,43 @@
+#-*- coding:utf-8 -*-
+
+import sys
+from PyQt4 import QtGui, QtCore
+
+class DelDialog(QtGui.QDialog):
+	def __init__(self, text = '', options = []):
+		super(DelDialog, self).__init__()
+		self.setWindowTitle(text)
+		self.options = options
+		self.short = []
+		self.initDialog()
+
+	def initDialog(self):
+		grid = QtGui.QGridLayout()
+		self.combo = QtGui.QComboBox()
+		for item in self.options:
+			self.short.append(item.split('\\')[-1])
+			self.combo.addItem(item.split('\\')[-1])
+		grid.addWidget(self.combo, 0, 0, 1, 2)
+		self.okBtn = QtGui.QPushButton('OK')
+		self.cancelBtn = QtGui.QPushButton('Cancel')
+		grid.addWidget(self.okBtn, 1, 0)
+		grid.addWidget(self.cancelBtn, 1, 1)
+		self.connect(self.okBtn, QtCore.SIGNAL('clicked()'), self.react)
+		self.connect(self.cancelBtn, QtCore.SIGNAL('clicked()'), self.react)
+		self.setLayout(grid)
+		with open('./style/dialog.css','r') as f:
+			self.setStyleSheet(f.read())
+
+	def react(self):
+		sender = self.sender()
+		if sender == self.okBtn:
+			res = str(self.combo.currentText())
+			try:
+				del self.options[self.short.index(res)]
+			except ValueError:
+				pass
+			with open('./data/main.txt', 'w', encoding = 'utf-8') as f:
+				f.write('\n'.join(self.options))
+			self.done(1)
+		else:
+			self.done(0)
